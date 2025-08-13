e567f872bdc7210f488ef3dbd1d962af4eddc435
unknown <leandro@pontogovsistemas.com.br>
2025-08-13 15:24:10 -0300

    feat: Etapa 1 desenvolvimento dos desafios extras

--- src/script.php
+++ src/script.php
@@ -11,14 +11,30 @@ else if($_POST["flGenero"] == "F")
 else
   $genero = "Outros";
 
-echo "Nome: " . $nome . " Idade: " . $idade . "  Genero: " . $genero . PHP_EOL;
+echo "Nome: " . $nome . " Idade: " . $idade . PHP_EOL;
+echo "Genero: " . $genero . PHP_EOL;
 
 $idadeDezAnos = $idade + 10;
 echo "Sua idade daqui 10 anos e: " . $idadeDezAnos . PHP_EOL;
 
 if($idade >= 18)
-  echo "Voce e maior de idade";
+  echo "Voce e maior de idade" . PHP_EOL;
 else
-  echo "Voce e menor de idade"
+  echo "Voce e menor de idade" . PHP_EOL;
+
+if(filter_var($idade, FILTER_VALIDATE_INT) === false){
+  echo "Sua idade nao e um inteiro" . PHP_EOL;
+}else{
+  echo "Sua idade e um inteiro" . PHP_EOL;
+}
+
+$ip = $_SERVER['REMOTE_ADDR'];
+echo "IP: " . $ip . PHP_EOL;
+
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+echo 'User Agent: '. $userAgent . PHP_EOL;
+
+$dataHora = date('Y-m-d H:i:s');
+echo "Data e Hora: " . $dataHora . PHP_EOL;
 
 ?>
